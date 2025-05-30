981b063acfdfa0523943595cfad02bd4ecc5735c
venkata501 <gondi.venkatasai@hcltech.com>
2025-05-30 18:31:52 +0530

    Delete playbook/Block-ExchangeIP.ps1

--- playbook/Block-ExchangeIP.ps1
+++
@@ -1,9 +0,0 @@
-Param
-(
-  [Parameter (Mandatory= $true)]
-  [String] $IPToBeBlocked
-)
-
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
-
-Add-IPBlockListEntry -IPAddress $IPToBeBlocked
